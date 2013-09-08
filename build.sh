9e632afca7db8f860b7405b3dac3a81029c12e9a
ian <ianbgraves@gmail.com>
2013-09-07 19:41:23 -0700

    Added the clover_service to the new build script and updated the apks

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
 #! /bin/sh
+
+set -e
+
+echo "Building Webview Shim"
 cd webview_shim
 android update project -p .
 ant clean debug
 cd ..
 mv webview_shim/bin/webview_shim-debug.apk apk/webview_shim.apk
+
+echo "Building Clover Service"
+cd clover_service
+android update project -p .
+ant clean debug
+cd ..
+mv clover_service/bin/clover_service-debug.apk apk/clover_service.apk
+
+echo "Done Building"
 
